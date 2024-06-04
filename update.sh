275aeb41ea5f3ccaf996254d2db0eec18bf35a7c
fangzheng liu <bitliufangzheng@gmail.com>
2024-06-03 20:40:36 -0400

    udated by FZ: reading nodes and send dat working, data to be parsed.

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 git pull --no-ff
 git status
 git add --all
-git commit -a -m "udated by FZ: solve I2C problem, to be tested the reading data from nodes."
+git commit -a -m "udated by FZ: reading nodes and send dat working, data to be parsed."
 git push
